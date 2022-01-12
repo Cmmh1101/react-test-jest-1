9fa8878e72921b9ac2f0d81206855e652449f12b
Carla <CMontano@greenmountaintechnology.com>
2022-01-12 14:59:24 -0600

    initial colo button testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// find an element with a role of a button and text of 'change color to blue'
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //  expect the background to color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe("Change to red");
 });
+
+// test("button turns blue when clicked", () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole("button", { name: "Change to blue" });
+// });
