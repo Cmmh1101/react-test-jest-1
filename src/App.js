9c550c6adb02c0e339041d3276555bdf3268ad46
Carla <CMontano@greenmountaintechnology.com>
2022-01-12 17:25:41 -0600

    checkbox disables and enables button

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import "./App.css";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   return (
     <div>
       <button
-        style={{ backgroundColor: buttonColor }}
+        style={{
+          backgroundColor: buttonColor,
+        }}
+        disabled={disabled}
         onClick={() => setButtonColor(newButtonColor)}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        area-checked={disabled}
+        onClick={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -20,6 +20,32 @@ test("button has correct initial color", () => {
   expect(colorButton.textContent).toBe("Change to red");
 });
 
+test("initial conditions", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  // check that the button starts out enabled
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox starts out unchecked
+  const checkBox = screen.getByRole("checkbox");
+  expect(checkBox).not.toBeChecked();
+});
+
+test("checkbox disables button on first click and enables on second click", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const checkBox = screen.getByRole("checkbox");
+
+  expect(colorButton).toBeEnabled();
+
+  fireEvent.click(checkBox);
+
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeEnabled();
+});
+
 // test("button turns blue when clicked", () => {
 //   render(<App />);
 //   const colorButton = screen.getByRole("button", { name: "Change to blue" });
